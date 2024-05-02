d6db653d65360e01d8eff6aad69c0c180ca89dd1
aditya <adityaischill3102@gmail.com>
2024-05-02 13:21:43 +0200

    Modified program to take input Modified program to sort list to alphabetical order Added "mbox.txt" to the folder

--- word_counter.py
+++ word_counter.py
@@ -2,9 +2,10 @@ import string
 
 
 counts = dict()
+fname = input("Which file do you want to open? ")
 
 try:
-    with open("story.txt") as filehandle:
+    with open(fname) as filehandle:
         for line in filehandle:
             line = line.rstrip()
             line = line.translate(line.maketrans(" "," ", string.punctuation))
@@ -15,7 +16,19 @@ try:
                     counts[word] += 1
                 else:
                     counts[word] = 1
-    print(counts)
-
+        
+    sort = input("Would you like me to sort this list for you in alphabetical order? ")
+    if sort == "YES":
+        sorted_list = list(counts.keys())
+        sorted_list.sort()
+        sorted_counts = dict()
+        for key in sorted_list:
+            sorted_counts[key] = counts[key]
+        print(sorted_counts)
+    
+    elif sort == "NO": 
+        print(counts)
+    
 except FileNotFoundError:
     print("The file you were looking for was not found.")
+    exit()
